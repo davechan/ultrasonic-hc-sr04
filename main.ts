b1f46f0d57b36d9b1e4f5b5bcef2924b68279d82
Dave Chan <davechan@users.noreply.github.com>
2021-03-21 02:10:42 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,18 @@
 let distance = 0
 basic.forever(function () {
-    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.analogWritePin(AnalogPin.P14, 116)
+    pins.analogWritePin(AnalogPin.P13, 124)
+})
+basic.forever(function () {
+    pins.digitalWritePin(DigitalPin.P8, 0)
     control.waitMicros(2)
-    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P8, 1)
     control.waitMicros(15)
-    pins.digitalWritePin(DigitalPin.P13, 0)
-    distance = Math.idiv(pins.pulseIn(DigitalPin.P13, PulseValue.High), 40)
-    basic.showNumber(distance)
+    pins.digitalWritePin(DigitalPin.P8, 0)
+    distance = Math.idiv(pins.pulseIn(DigitalPin.P2, PulseValue.High), 40)
+    led.plotBarGraph(
+    distance,
+    30
+    )
     basic.pause(100)
 })
